a4efa08a130c4c371fd51eea9a547f1c0e7688e0
WaDoMa <github.20.mkmb@spamgourmet.com>
2015-11-08 22:27:24 +0100

    added bricks and reflection by angle

--- Pongo/Pongo.ino
+++ Pongo/Pongo.ino
@@ -5,10 +5,17 @@
 #include <TFT.h>            // Arduino LCD library
 #include <SPI.h>
 
+#define MAX_NUM_BRICKS 40
 
 struct screen
 {
   int resolution_h,resolution_v;
+};
+
+struct brick
+{
+  int16_t x,y;
+  bool alive;
 };
 
 struct ball
@@ -31,14 +38,23 @@ struct paddle
 screen scr={};
 ball ball1={};
 paddle paddle1={};
-
+brick bricks[MAX_NUM_BRICKS];
+  
 void setup()
 {
   EsploraTFT.begin();   // initialize the display
   EsploraTFT.background(0, 0, 0);  // set the background the black
   scr = {EsploraTFT.width(),EsploraTFT.height()};  //determine and store resolution of screen
-  ball1 = {0,0,NULL,NULL,1.1,1.5,5,5,255,255,255};  //initialize ball position to upper left corner, ball trajectory, dimensions and color
-  paddle1 = {scr.resolution_h/2,scr.resolution_v/2,NULL,NULL,20,5,255,255,255};  //initialize paddle position to the middle of the screen, dimensions and color
+  ball1 = {0,0,0,0,1.1,1.5,5,5,255,255,255};  //initialize ball position to upper left corner, ball trajectory, dimensions and color
+  paddle1 = {scr.resolution_h/2,scr.resolution_v/2,0,0,20,5,255,255,255};  //initialize paddle position to the middle of the screen, dimensions and color
+  int i=0;
+  for(int16_t x=0;x < scr.resolution_h; x=x+20 )
+  {
+    bricks[i].x = x;
+    bricks[i].y = 20;
+    bricks[i].alive=true;
+    i++;
+  }
 }
 
 void loop()
@@ -57,16 +73,14 @@ void loop()
   paddle1.previous_y = paddle1.y;
   
   ballmovementintervall = map(Esplora.readSlider(), 0, 1023, 0, 80) + 1;   // Reads and maps the slider position to determine the speed of the ball. Here, "80" results into the minimum speed and "0" into the maximum speed.
-  Serial.print(millis());  //output millis for debugging
-  Serial.print("\t");
-  Serial.print(ballSpeed);  //output ball speed for debugging
-  Serial.print("\t");
-  Serial.print(millis() % ballSpeed);  //output calc for debugging
-  Serial.print("\n");
+  //Serial.print(millis());  //output millis for debugging
+  //Serial.print("\t");
+  //Serial.print("\n");
   if (millis() % ballmovementintervall < 2)  //conditon gets periodically TRUE as times goes by 
   {
     moveBall();
   }
+  drawBricks();
 }
 
 // this function determines the ball's position on screen
@@ -82,14 +96,28 @@ void moveBall()
   }
 
   // check if the ball and the paddle are occupying the same space on screen
-  if (inPaddle(ball1.x, ball1.y, paddle1.x, paddle1.y, paddle1.width, paddle1.height))
+  if (inRect(ball1.x, ball1.y, paddle1.x, paddle1.y, paddle1.width, paddle1.height))
   {
-    ball1.delta_y = -ball1.delta_y;
+    double paddlehitoffcenter = (paddle1.x + paddle1.width / 2.0f) - ball1.x;
+    double deflection = paddlehitoffcenter / (paddle1.width / 2.0f);
+//    Serial.print(deflection);
+//    Serial.print("\t");
+    ball1.delta_x = deflection*2.0f;
+    ball1.delta_y = -2.0f;
   }
 
   ball1.x += ball1.delta_x;  // update the ball's position
   ball1.y += ball1.delta_y;
 
+  for(int16_t i = 0;i < MAX_NUM_BRICKS;i++)
+  {
+    if( ( bricks[i].alive == true) && inRect(ball1.x, ball1.y, bricks[i].x, bricks[i].y, 10, 2) )
+    { 
+       bricks[i].alive = false;
+       EsploraTFT.fill(0, 0, 0);
+       EsploraTFT.rect(bricks[i].x, bricks[i].y, 10, 2);
+    }
+  }
   EsploraTFT.fill(0, 0, 0);// erase the ball's previous position
   if (ball1.previous_x != ball1.x || ball1.previous_y != ball1.y)
   {
@@ -112,7 +140,7 @@ void moveBall()
 }
 
 // this function checks the position of the ball to see if it intersects with the paddle
-boolean inPaddle(int x, int y, int rectX, int rectY, int rectWidth, int rectHeight)
+boolean inRect(int x, int y, int rectX, int rectY, int rectWidth, int rectHeight)
 {
  if ((x >= rectX && x <= (rectX + rectWidth)) && (y >= rectY && y <= (rectY + rectHeight)))
   {
@@ -122,6 +150,17 @@ boolean inPaddle(int x, int y, int rectX, int rectY, int rectWidth, int rectHeig
   {
     return false;
   }
+}
+
+void drawBricks()
+{
+    for(int16_t i=0;i < MAX_NUM_BRICKS;i++)
+    {
+      if(bricks[i].alive == true)
+      {
+          EsploraTFT.rect(bricks[i].x, bricks[i].y, 10, 2);
+      }
+    }
 }
 
 
